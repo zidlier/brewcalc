80561185ac64cf5fadf32f25b0515b9881a4c150
zidlier <38188145+zidlier@users.noreply.github.com>
2022-11-08 12:20:46 +0000

    Deploying to gh-pages from  @ 0550ef4d77ce9c979924642dd784f447eba61405 🚀

---
+++ precache-manifest.9c71371c397d1afca50638985065b9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9583816cdd3779a99b62bcaad3ea4c",
+    "url": "/brewcalc/index.html"
+  },
+  {
+    "revision": "8ecf605457cccd6393bf",
+    "url": "/brewcalc/static/css/main.801c87dc.chunk.css"
+  },
+  {
+    "revision": "60cade93822852b483b4",
+    "url": "/brewcalc/static/js/2.eaff7836.chunk.js"
+  },
+  {
+    "revision": "5ed92eff82e22231cb96ec7cb280983a",
+    "url": "/brewcalc/static/js/2.eaff7836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecf605457cccd6393bf",
+    "url": "/brewcalc/static/js/main.f2ccf912.chunk.js"
+  },
+  {
+    "revision": "a26396c003b4ecf102af",
+    "url": "/brewcalc/static/js/runtime-main.27e8bc2d.js"
+  }
+]);
